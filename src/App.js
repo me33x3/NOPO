bf0370faf00088889a71137f7068bf445ac8d741
srookie-yoon <srookie.git@gmail.com>
2021-11-13 23:52:35 +0900

    server build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, {  useEffect }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+// axios 추가
+import axios from 'axios';
+
 function App() {
+
+  // 서버에서 받은 데이터를 console로 찍어서 확인한다.
+  useEffect(() => {
+    axios.get('/api/test')
+      .then(res => console.log(res))
+      .catch()
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
