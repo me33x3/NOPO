9939bfbc158955ced2f830f939810431dfce827d
me33x3 <me33x3@gmail.com>
2021-11-25 23:06:21 +0900

    create main page

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function Login() {
+    return (
+        <Fragment>
+            <h2>Login Page</h2>
+        </Fragment>
+    );
+}
+
+export default Login;
---
+++ src/pages/Main.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function Main() {
+    return (
+        <Fragment>
+            <h2>Main Page</h2>
+        </Fragment>
+    );
+}
+
+export default Main;
